639a8820f61eeb00eed5407fc66af2666cc224dd
Okeke Chidiogo <okekechidiogoc@gmail.com>
2025-10-19 20:05:17 +0100

    Initial Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // Find the element to display the time
+  const timeElement = document.querySelector('[data-testid="test-user-time"]');
+
+  function updateTime() {
+    if (timeElement) {
+      timeElement.textContent = Date.now();
+    }
+  }
+
+  updateTime();
+
+  setInterval(updateTime, 1000);
+});
